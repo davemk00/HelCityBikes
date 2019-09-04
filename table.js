83685bff005ac8a2753d0f28d254d54ff4fc975b
David K <david.king@zoho.com>
2019-09-04 10:47:50 +0300

    Update table.js

--- table.js
+++ table.js
@@ -9,16 +9,20 @@ function writeTable(coords)	{
 	
 	document.getElementById('stationText').innerHTML = ''; // clear table
 	
+	document.getElementById('stationsInRangeTable').innerHTML +=
+		"<table style='width:50%'>" + 
+		"<tr><th>ID</th><th>Name</th><th>Bikes free/Total</th></tr>"
+		//<th>Distance/Total</th>
 	for (var i = 0; i < stationsInRangeArray.length; i++) {
-		
-		document.getElementById('stationText').innerHTML += 
-			"id = " + stationsInRangeArray[i].extra.uid + ", text = " + 
-			stationsInRangeArray[i].name + "  " + 
-			stationsInRangeArray[i].free_bikes + "/" + (
-			stationsInRangeArray[i].empty_slots+
-			stationsInRangeArray[i].free_bikes) + "<br />";	
+		document.getElementById('stationsInRangeTable').innerHTML += "<tr>" + 
+				"<td>" + stationsInRangeArray[i].extra.uid + "</td>" + 
+				"<td>" + stationsInRangeArray[i].name + "</td>" + 
+				"<td>" + stationsInRangeArray[i].free_bikes + "/" + (stationsInRangeArray[i].empty_slots+stationsInRangeArray[i].free_bikes) + "</td>" + 
+			"</tr>";
 	}
+	document.getElementById('stationsInRangeTable').innerHTML += "</table>";
 }
+
 
 
 
